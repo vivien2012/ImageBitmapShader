073af7895d8a417d1f4ec631a6c0b5e6a1b5625b
yaolingyun <754056109@qq.com>
2017-01-19 15:26:10 +0800

    Initial commit

---
+++ app/src/main/java/com/vivien/shader/App.java
@@ -0,0 +1,15 @@
+package com.vivien.shader;
+
+import android.app.Application;
+
+/**
+ * Created by vivien on 17/1/13.
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
